e7868a742bd4a5fb0ba6f8260aa646322f1aab14
deepspacediver <luka.koscik@gmail.com>
2022-05-15 08:20:40 +0200

    Check password functionality and styling

--- java.js
+++ java.js
@@ -1,2 +1,20 @@
-let userPasswordInput = document.getElementById('user-password')
-let userPasswordConfrimation = document.getElementById('user-confirm_password')
+const userPasswordInput = document.getElementById('user-password')
+const userPasswordConfrimation = document.getElementById('user-confirm_password')
+const spanErrorPopup = document.querySelector('.errorHolder')
+
+function checkPassword(){
+    password = userPasswordInput.value;
+    passwordConfirm = userPasswordConfrimation.value;
+    if(password != passwordConfirm){
+        spanErrorPopup.classList.add('invalidPassword') 
+        console.log('yo')
+    }else{
+        spanErrorPopup.classList.remove('invalidPassword');
+    }
+
+}
+
+
+userPasswordInput.onchange = checkPassword;
+userPasswordConfrimation.onchange =checkPassword;
+
